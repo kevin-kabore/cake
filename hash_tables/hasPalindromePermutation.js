eed334c4c42b7f534f982a01433a91a7d91a61a1
Kevin Kabore <kevin@tvgla.com>
2019-06-25 18:00:27 -0700

    Adds tests

--- hash_tables/hasPalindromePermutation.js
+++ hash_tables/hasPalindromePermutation.js
@@ -27,17 +27,97 @@
 function hasPalindromePermutation(theString) {
 
     let unpairedChars = new Set();
-     for(let char of theString) {
-       if(unpairedChars.has(char)) {
-         unpairedChars.delete(char);
-       } else {
-         unpairedChars.add(char);
-       }
-     }
+    for(let char of theString) {
+        if(unpairedChars.has(char)) {
+            unpairedChars.delete(char);
+        } else {
+            unpairedChars.add(char);
+        }
+    }
      
     if (unpairedChars.size <= 1) return true;
     
     return false;
-  }
+}
   
+
+// Tests
+
+let desc = 'permutation with odd number of chars';
+assertEqual(hasPalindromePermutation('aabcbcd'), true, desc);
+
+desc = 'permutation with even number of chars';
+assertEqual(hasPalindromePermutation('aabccbdd'), true, desc);
+
+desc = 'no permutation with odd number of chars';
+assertEqual(hasPalindromePermutation('aabcd'), false, desc);
+
+desc = 'no permutation with even number of chars';
+assertEqual(hasPalindromePermutation('aabbcd'), false, desc);
+
+desc = 'empty string';
+assertEqual(hasPalindromePermutation(''), true, desc);
+
+desc = 'one character string ';
+assertEqual(hasPalindromePermutation('a'), true, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
+// Cleaner approach: "keep two pointers" pattern.
+// > use obj to get count of each char, should be even for all, except for one if string is not even
+
+// Even cleaner: only need to track odd chars. Can use a set.
+// If encounter it first time, add to set
+// If second time, remove from set
+// At end, check that size of set is <= 1; If so, return true
+
+function hasPalindromePermutation(theString) {
+
+    let unpairedChars = new Set();
+    for(let char of theString) {
+        if(unpairedChars.has(char)) {
+            unpairedChars.delete(char);
+        } else {
+            unpairedChars.add(char);
+        }
+    }
+     
+    if (unpairedChars.size <= 1) return true;
+    
+    return false;
+}
+  
+
+// Tests
+
+let desc = 'permutation with odd number of chars';
+assertEqual(hasPalindromePermutation('aabcbcd'), true, desc);
+
+desc = 'permutation with even number of chars';
+assertEqual(hasPalindromePermutation('aabccbdd'), true, desc);
+
+desc = 'no permutation with odd number of chars';
+assertEqual(hasPalindromePermutation('aabcd'), false, desc);
+
+desc = 'no permutation with even number of chars';
+assertEqual(hasPalindromePermutation('aabbcd'), false, desc);
+
+desc = 'empty string';
+assertEqual(hasPalindromePermutation(''), true, desc);
+
+desc = 'one character string ';
+assertEqual(hasPalindromePermutation('a'), true, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
   
