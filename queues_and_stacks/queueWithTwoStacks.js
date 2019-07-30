e4bd3f67c335ee2a908f1f88367851ad064de284
Kevin Kabore <kevin@tvgla.com>
2019-07-30 11:39:12 -0700

    Adds tests

--- queues_and_stacks/queueWithTwoStacks.js
+++ queues_and_stacks/queueWithTwoStacks.js
@@ -10,30 +10,30 @@
  * 
  */
 
- class QueueTwoStacks {
-     constructor() {
-         this.pushStack = new Stack();
-         this.popStack = new Stack();
-     }
+class QueueTwoStacks {
+    constructor() {
+        this.pushStack = new Stack();
+        this.popStack = new Stack();
+    }
 
-     enqueue(item) {
-         this.pushStack.push(item);
-     }
+    enqueue(item) {
+        this.pushStack.push(item);
+    }
 
-     dequeue() {
-         if(this.popStack.size === 0) {
-            
-            while(this.pushStack.size > 0) {
-                let item = this.pushStack.pop();
-                this.popStack.push(item);
-            }
+    dequeue() {
+        if(this.popStack.size === 0) {
+        
+        while(this.pushStack.size > 0) {
+            let item = this.pushStack.pop();
+            this.popStack.push(item);
+        }
 
-            if (this.popStack.size === 0) throw new Error('Queue is Empty');
-         }
-         return this.popStack.pop();
-     }
+        if (this.popStack.size === 0) throw new Error('Queue is Empty');
+        }
+        return this.popStack.pop();
+    }
 
- }
+}
 
  class StackNode {
     constructor(item) {
@@ -43,13 +43,13 @@
     }
  }
 
- class Stack {
+class Stack {
     constructor() {
         this.top = null;
         this.bottom = null;
         this.size = 0;
     }
-    
+
     push(item) {
         let newNode = new StackNode(item);
         if(!this.top) {
@@ -63,7 +63,7 @@
         this.size++;
         return this;
     }
-    
+
     pop() {
         let top = this.top;
         if(!top) return null;
@@ -74,9 +74,61 @@
         this.size--;
         return top.data;
     }
-    
+
     join(topNode, bottomNode) {
         if(bottomNode !== null) bottomNode.above = topNode;
         if(topNode !== null) topNode.below = bottomNode;
     }
- }
+}
+
+
+
+ // Tests
+const q = new QueueTwoStacks();
+
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+
+let desc = 'dequeue #1';
+let actual = q.dequeue();
+let expected = 1;
+assertEquals(actual, expected, desc);
+
+desc = 'dequeue #2';
+actual = q.dequeue();
+expected = 2;
+assertEquals(actual, expected, desc);
+
+q.enqueue(4);
+
+desc = 'dequeue #3';
+actual = q.dequeue();
+expected = 3;
+assertEquals(actual, expected, desc);
+
+desc = 'dequeue #4';
+actual = q.dequeue();
+expected = 4;
+assertEquals(actual, expected, desc);
+
+desc = 'dequeue from empty queue';
+const emptyDequeue = () => q.dequeue();
+assertThrowsError(emptyDequeue, desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
