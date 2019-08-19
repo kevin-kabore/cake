4dc3f0adcf5a5f1fd135197f57540aacf8e6aefd
Kevin Kabore <kevin@tvgla.com>
2019-08-19 12:04:11 -0700

    Fixes typo, adds tests and InterviewCake solution

--- arrays/mergeArrays.js
+++ arrays/mergeArrays.js
@@ -1,24 +1,102 @@
-function mergeArrays(arr1, arr2) {
-  let length1 = arr1.length,
-    length2 = arr2.length,
-    i = 0,
-    j = 0,
-    result = [];
+function mergeArrays(myArr, alicesArr) {
 
-    while (i < length1 && j < length2) {
-      if (arr1[i] < arr2[j]) {
-        result.push(arr[i]);
-        i++;
-      }
-      if (arr1[i] > arr2[j]) {
-        result.push(arr2[j]);
-        j++;
-      }
-      if(arr1[i] === arr2[j]) {
-        result.push(arr1[i], arr[j]);
-        i++;
-        j++;
-      }
+  // Combine the sorted arrays into one large sorted array
+  
+  const mergedArr = [];
+  
+  let i = 0, j = 0;
+  
+  while(i < myArr.length && j < alicesArr.length) {
+    if(myArr[i] < alicesArr[j]) {
+      mergedArr.push(myArr[i]);
+      i++;
+    } 
+    if(alicesArr[j] < myArr[i]) {
+      mergedArr.push(alicesArr[j]);
+      j++;
+    } 
+    if(myArr[i] === alicesArr[j]){
+      mergedArr.push(myArr[i]);
+      mergedArr.push(alicesArr[j]);
+      i++;
+      j++;
     }
-    return result.concat(arr1.slice(i), arr2.slice(j))
+  }
+  return mergedArr.concat(myArr.slice(i), alicesArr.slice(j));
+
+}
+
+/** Interview Cake Solution */
+function mergeArrays(myArray, alicesArray) {
+
+  // Set up our mergedArray
+  const mergedArray = [];
+
+  let currentIndexAlices = 0;
+  let currentIndexMine = 0;
+  let currentIndexMerged = 0;
+
+  while (currentIndexMerged < (myArray.length + alicesArray.length)) {
+
+    const isMyArrayExhausted = currentIndexMine >= myArray.length;
+    const isAlicesArrayExhausted = currentIndexAlices >= alicesArray.length;
+
+    // Case: next comes from my array
+    // My array must not be exhausted, and EITHER:
+    // 1) Alice's array IS exhausted, or
+    // 2) The current element in my array is less
+    //    than the current element in Alice's array
+    if (!isMyArrayExhausted && (isAlicesArrayExhausted ||
+      (myArray[currentIndexMine] < alicesArray[currentIndexAlices]))) {
+
+      mergedArray[currentIndexMerged] = myArray[currentIndexMine];
+      currentIndexMine++;
+
+      // Case: next comes from Alice's array
+    } else {
+      mergedArray[currentIndexMerged] = alicesArray[currentIndexAlices];
+      currentIndexAlices++;
+    }
+
+    currentIndexMerged++;
+  }
+
+  return mergedArray;
+}
+
+// Tests
+
+let desc = 'both arrays are empty';
+let actual = mergeArrays([], []);
+let expected = [];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'first array is empty';
+actual = mergeArrays([], [1, 2, 3]);
+expected = [1, 2, 3];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'second array is empty';
+actual = mergeArrays([5, 6, 7], []);
+expected = [5, 6, 7];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'both arrays have some numbers';
+actual = mergeArrays([2, 4, 6], [1, 3, 7]);
+expected = [1, 2, 3, 4, 6, 7];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'arrays are different lengths';
+actual = mergeArrays([2, 4, 6, 8], [1, 7]);
+expected = [1, 2, 4, 6, 7, 8];
+assertDeepEqual(actual, expected, desc);
+
+function assertDeepEqual(a, b, desc) {
+  const aStr = JSON.stringify(a);
+  const bStr = JSON.stringify(b);
+  if (aStr !== bStr) {
+    console.log(`${desc} ... FAIL: ${aStr} != ${bStr}`);
+  } else {
+    console.log(`${desc} ... PASS`);
+  }
 }
